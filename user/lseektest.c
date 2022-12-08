5b04b31ac8b3d7d867ab0f2d9a8029f826667afd
koh <kohbanye@gmail.com>
2022-12-08 16:29:29 +0900

    add test program for lseek

---
+++ user/lseektest.c
@@ -0,0 +1,31 @@
+#include "kernel/types.h"
+#include "user/user.h"
+#include "kernel/fcntl.h"
+
+int main() {
+  char buf[32];
+
+  int fd = open("test.txt", O_WRONLY | O_CREATE);
+  write(fd, "hello", 5);
+  lseek(fd, 0, SEEK_SET);
+  write(fd, "!", 1);
+  close(fd);
+
+  fd = open("test.txt", O_RDONLY);
+  read(fd, buf, 32);
+  printf("%s\n", buf);
+  close(fd);
+
+  fd = open("test.txt", O_WRONLY | O_TRUNC);
+  write(fd, "hello", 5);
+  lseek(fd, -2, SEEK_CUR);
+  write(fd, "!", 1);
+  close(fd);
+
+  fd = open("test.txt", O_RDONLY);
+  read(fd, buf, 32);
+  printf("%s\n", buf);
+  close(fd);
+
+  exit(0);
+}
